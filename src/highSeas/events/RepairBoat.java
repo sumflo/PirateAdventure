3720524197ebb8d1444dcfeb427c1d7737100c0d
sumflo <real61madrid@gmail.com>
2021-08-24 23:44:25 +0200

    update

--- src/highSeas/events/RepairBoat.java
+++ src/highSeas/events/RepairBoat.java
@@ -4,32 +4,38 @@ import highSeas.crew.Captain;
 import highSeas.enums.TreasureType;
 import highSeas.ship.Ship;
 
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class RepairBoat {
 
     public void repairBoat(Captain captain){
 
-        int item = 0;
-
         System.out.println("A small ship appears on the horizon. Blue flag shows saw and hammer. This is a repair ship! ");
-        System.out.println();
         System.out.println("Now you have the opportunity to repair and improve your ship.");
+        System.out.println("Think carefully about your choice, there is only one interaction possible in this round!");
+        System.out.println();
+
         captain.getShip().showCargo();
+        System.out.println();
+
+        int interaction = getChoiceFromRepairBoatMenu(captain.getShip());
+        System.out.println();
 
     }
 
-    private void repairBoatMenu(Ship captainsShip){
+    /** Felajánlja a lehetséges interakciókat attól függően, hogy mi a játékos hajójának a rakománya.
+     *  Ezt addig ismétli, amíg valid értéket nem kap inputként.
+     *  Majd visszaadja a választott interakció értékét.*/
+    private int getChoiceFromRepairBoatMenu(Ship captainsShip){
 
         Scanner scanner = new Scanner(System.in);
-        int interaction = 0;
+        int interaction;
 
         System.out.println("In exchange for your treasures, curses, equipments  and currencies, you can choose from " +
                 "the following interactions:");
+        System.out.println();
 
-       // do{
+       do{
             if(offerRepairAndFillRumBoatInteraction(captainsShip) && // ha van ágyú, rum, akkor fizetni is lehet, és ágyút, rumot szerelni/tölteni
                     offerRepairAndInstallCannonBoatInteraction(captainsShip)){
                 System.out.println( "(0) Exit the event.\n" +
@@ -38,45 +44,83 @@ public class RepairBoat {
                         "(3) Fill the captain's rum storage.\n");
             }else if(offerRepairAndFillRumBoatInteraction(captainsShip) && // ha van rum, akkor fizetni, rumot tölteni lehet, de ágyút szerelni nem
                     !offerRepairAndInstallCannonBoatInteraction(captainsShip)){
-                //valami
+                System.out.println( "(0) Exit the event.\n" +
+                        "(1) Increase/repair the Hit Point of the ship.\n"+
+                        "(NOT ALLOWED NOW) Install cannons. \n"+
+                        "(3) Fill the captain's rum storage.\n");
             }else if(!offerRepairAndFillRumBoatInteraction(captainsShip) && // ha van ágyú, akkor fizetni, ágyút szerelni lehet, de rumot tölten nem
                     offerRepairAndInstallCannonBoatInteraction(captainsShip)){
-                //valami
-            } // ha van fizetőeszköz, vagy kincs, vagy átok, de nincs ágyú sem, és rum sem, akkor csak fizetni lehet
+                System.out.println( "(0) Exit the event.\n" +
+                        "(1) Increase/repair the Hit Point of the ship.\n"+
+                        "(2) Install cannons. \n"+
+                        "(NOT ALLOWED NOW) Fill the captain's rum storage.\n");
+            }else if(offerOnlyRepairBoatInteraction(captainsShip)){ // ha van fizetőeszköz, vagy kincs, vagy átok, de nincs ágyú sem, és rum sem, akkor csak fizetni lehet
+                System.out.println( "(0) Exit the event.\n" +
+                        "(1) Increase/repair the Hit Point of the ship.\n"+
+                        "(NOT ALLOWED NOW) Install cannons. \n"+
+                        "(NOT ALLOWED NOW) Fill the captain's rum storage.\n");
+            }else{ // ha nincs semilyen fizetésre alkalmas eszköz, akkor csak az eventből lehet kilépni, és befejezni a kört.
+                System.out.println( "(0) Exit the event.\n" +
+                        "(NOT ALLOWED NOW) Increase/repair the Hit Point of the ship.\n"+
+                        "(NOT ALLOWED NOW) Install cannons. \n"+
+                        "(NOT ALLOWED NOW) Fill the captain's rum storage.\n");
+            }
 
-       // } while (ameddig valid nem lesz a beadott interakció );
+            interaction = scanner.nextInt();
 
+       } while (!isValidInteraction(interaction, captainsShip));
+
+        return interaction;
     }
 
-    /* Rájöttem, hogy ez felesleges és ráadásul rossz is arra a célra, amit én szeretnék. */
+    private void executeInteraction(int interaction, Ship captainsShip){
+
+        if (interaction == 0){
+            System.out.println(captainsShip.getName() + " continued on its way to another adventure. What will tomorrow bring to the captain?");
+        }else if(interaction == 1){
+
+        }else if(interaction == 2){
+
+        }else if(interaction == 3){
+
+        }
+    }
+
+    /** Megvizsgálja, hogy a rakomyányban nincs-e EQUIPMENT típusú kincs, viszont van-e bármilyen más olyan típusú,
+     * ami nem SYMBOL( a value-ja minden SYMBOL-nak 0, azaz nem lehet "fizetni" vele) */
     private boolean offerOnlyRepairBoatInteraction(Ship captainsShip){
 
-        Set<TreasureType> kind = new HashSet<>();
+        int kind = 0;
 
         for (int i = 0; i < captainsShip.getCargo().size(); i++) {
             if(captainsShip.getCargo().get(i).getType().equals(TreasureType.EQUIPMENT)){
-                kind.add(TreasureType.EQUIPMENT);
+                return false;
             }else if(captainsShip.getCargo().get(i).getType().equals(TreasureType.CURRENCY)){
-                kind.add(TreasureType.CURRENCY);
+                kind++;
             }else if(captainsShip.getCargo().get(i).getType().equals(TreasureType.TREASURE)){
-                kind.add(TreasureType.TREASURE);
+                kind++;
             }else if(captainsShip.getCargo().get(i).getType().equals(TreasureType.CURSE)) {
-                kind.add(TreasureType.TREASURE);
+                kind++;
             }
         }
 
-        // ha nem tartalmaz equipmentet, de bármelyik másik hármat
+        if(kind > 0){
+            return true;
+        }
 
-        return kind.size() == 3;
+        return false;
     }
 
+    /** Megvizsgálja, hogy a rakományban van-e olyan nevű kincs, aminek a neve tartalmazza a 'rum' szót
+     * -->> lehetséges a rum feltöltése */
     private boolean offerRepairAndFillRumBoatInteraction(Ship captainsShip){
 
         boolean haveRum = false;
         int rum = 0;
 
         for (int i = 0; i < captainsShip.getCargo().size(); i++) {
-            if(captainsShip.getCargo().get(i).getName().contains("rum")){
+            if(captainsShip.getCargo().get(i).getName().contains("rum") &&  // fill-full rum barrels --> symbol
+                    captainsShip.getCargo().get(i).getType().equals(TreasureType.EQUIPMENT)){
                 rum++;
             }
         }
@@ -88,6 +132,8 @@ public class RepairBoat {
         return haveRum;
     }
 
+    /** Megvizsgálja, hogy a rakományban van-e olyan nevű kincs, aminek a neve tartalmazza a 'cannon' szót
+     * -->> lehetséges az ágyúk felszerelése */
     private boolean offerRepairAndInstallCannonBoatInteraction(Ship captainsShip){
 
         boolean haveCannon = false;
@@ -105,4 +151,27 @@ public class RepairBoat {
 
         return haveCannon;
     }
+
+    /** Megvizsgálja, hogy a választott interakció a rakományhoz képest valid-e, választható-e. */
+    private boolean isValidInteraction(int interaction, Ship captainsShip){
+
+        if(offerRepairAndFillRumBoatInteraction(captainsShip) &&
+                offerRepairAndInstallCannonBoatInteraction(captainsShip) && interaction > -1 && interaction < 4){
+            return true;
+        }else if(offerRepairAndFillRumBoatInteraction(captainsShip) &&
+                !offerRepairAndInstallCannonBoatInteraction(captainsShip) && interaction > -1 && interaction < 4 && interaction != 2){
+           return true;
+        }else if(!offerRepairAndFillRumBoatInteraction(captainsShip) &&
+                offerRepairAndInstallCannonBoatInteraction(captainsShip) && interaction > -1 && interaction < 3){
+            return true;
+        }else if(offerOnlyRepairBoatInteraction(captainsShip) && interaction > -1 && interaction < 2){
+            return true;
+        }else if (interaction == 0){ // ez lehet, hogy szükségtelen?
+            return true;
+        }else{
+            System.out.println("The chosen interaction is invalid.");
+            return false;
+        }
+    }
+
 }
